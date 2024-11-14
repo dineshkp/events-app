4e0c634bc36bb3c820c4acdb3a512459c4619341
Dinesh Kumar Ponnusamy <git@dineshkp.com>
2024-11-14 15:42:23 +0530

    feat: add message constants

---
+++ src/constants/messages.ts
@@ -0,0 +1,13 @@
+export const MESSAGES = {
+    SUCCESS: {
+        EVENT_CREATED: 'Event created successfully',
+        EVENT_UPDATED: 'Event updated successfully',
+        EVENT_DELETED: 'Event deleted',
+    },
+    ERROR: {
+        GENERIC: 'Something went wrong',
+    },
+    ACTIONS: {
+        UNDO: 'Undo',
+    },
+} as const
